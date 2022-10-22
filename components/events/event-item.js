9f0d1d3633bbad5fca9fa19f2308471bb3a03391
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-10-22 19:02:42 +0600

    add feature events

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import classes from './event-item.module.css'
+import Button from "../ui/button";
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -10,20 +12,20 @@ const EventItem = (props) => {
   const formatedAdress = location.replace(",", "\n");
   const exploreLink = `/event/${id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
-          <h2>TITLE</h2>
-          <div>
-            <time>Date</time>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
           </div>
-          <div>
-            <address>ADDRESS</address>
+          <div className={classes.address}>
+            <address>{location}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
